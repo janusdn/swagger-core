31d8fac0172fdf7dd17758de93bcff109a5e0fc9
Janus Dam Nielsen <jdn@fagidiot.dk>
2015-12-18 14:52:40 +0100

    A potential fix for #1103. Do not rely on ServletContext to initailize swagger instance.

--- modules/swagger-jaxrs/src/main/java/io/swagger/jaxrs/config/BeanConfig.java
+++ modules/swagger-jaxrs/src/main/java/io/swagger/jaxrs/config/BeanConfig.java
@@ -1,5 +1,20 @@
 package io.swagger.jaxrs.config;
 
+import java.util.Collection;
+import java.util.HashSet;
+import java.util.LinkedHashSet;
+import java.util.Set;
+
+import org.apache.commons.lang3.StringUtils;
+import org.reflections.Reflections;
+import org.reflections.scanners.ResourcesScanner;
+import org.reflections.scanners.SubTypesScanner;
+import org.reflections.scanners.TypeAnnotationsScanner;
+import org.reflections.util.ClasspathHelper;
+import org.reflections.util.ConfigurationBuilder;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
 import io.swagger.annotations.Api;
 import io.swagger.annotations.SwaggerDefinition;
 import io.swagger.config.FilterFactory;
@@ -13,278 +28,306 @@ import io.swagger.models.Info;
 import io.swagger.models.License;
 import io.swagger.models.Scheme;
 import io.swagger.models.Swagger;
-import org.apache.commons.lang3.StringUtils;
-import org.reflections.Reflections;
-import org.reflections.scanners.ResourcesScanner;
-import org.reflections.scanners.SubTypesScanner;
-import org.reflections.scanners.TypeAnnotationsScanner;
-import org.reflections.util.ClasspathHelper;
-import org.reflections.util.ConfigurationBuilder;
-import org.slf4j.Logger;
-import org.slf4j.LoggerFactory;
 
-import java.util.HashSet;
-import java.util.Set;
+public class BeanConfig extends AbstractScanner implements Scanner, SwaggerConfig, ReaderConfig {
+	Logger LOGGER = LoggerFactory.getLogger(BeanConfig.class);
 
-public class BeanConfig extends AbstractScanner implements Scanner, SwaggerConfig {
-    Logger LOGGER = LoggerFactory.getLogger(BeanConfig.class);
+	Reader reader = new Reader(new Swagger());
 
-    Reader reader = new Reader(new Swagger());
+	String resourcePackage;
+	String[] schemes;
+	String title;
+	String version;
+	String description;
+	String termsOfServiceUrl;
+	String contact;
+	String license;
+	String licenseUrl;
+	String filterClass;
 
-    String resourcePackage;
-    String[] schemes;
-    String title;
-    String version;
-    String description;
-    String termsOfServiceUrl;
-    String contact;
-    String license;
-    String licenseUrl;
-    String filterClass;
+	Info info;
+	String host;
+	String basePath;
 
-    Info info;
-    String host;
-    String basePath;
+	boolean isScanAllResources;
 
-    public String getResourcePackage() {
-        return this.resourcePackage;
-    }
+	Set<String> ignoredRoutes = new LinkedHashSet<String>();
 
-    public void setResourcePackage(String resourcePackage) {
-        this.resourcePackage = resourcePackage;
-    }
+	public String getResourcePackage() {
+		return this.resourcePackage;
+	}
 
-    public String[] getSchemes() {
-        return schemes;
-    }
+	public void setResourcePackage(String resourcePackage) {
+		this.resourcePackage = resourcePackage;
+	}
 
-    public void setSchemes(String[] schemes) {
-        this.schemes = schemes;
-    }
+	public String[] getSchemes() {
+		return this.schemes;
+	}
 
-    public String getTitle() {
-        return title;
-    }
+	public void setSchemes(String[] schemes) {
+		this.schemes = schemes;
+	}
 
-    public void setTitle(String title) {
-        this.title = title;
-    }
+	public String getTitle() {
+		return this.title;
+	}
 
-    public String getVersion() {
-        return version;
-    }
+	public void setTitle(String title) {
+		this.title = title;
+	}
 
-    public void setVersion(String version) {
-        this.version = version;
-    }
+	public String getVersion() {
+		return this.version;
+	}
 
-    public String getDescription() {
-        return description;
-    }
+	public void setVersion(String version) {
+		this.version = version;
+	}
 
-    public void setDescription(String description) {
-        this.description = description;
-    }
+	public String getDescription() {
+		return this.description;
+	}
 
-    public String getTermsOfServiceUrl() {
-        return termsOfServiceUrl;
-    }
+	public void setDescription(String description) {
+		this.description = description;
+	}
 
-    public void setTermsOfServiceUrl(String termsOfServiceUrl) {
-        this.termsOfServiceUrl = termsOfServiceUrl;
-    }
+	public String getTermsOfServiceUrl() {
+		return this.termsOfServiceUrl;
+	}
 
-    public String getContact() {
-        return contact;
-    }
+	public void setTermsOfServiceUrl(String termsOfServiceUrl) {
+		this.termsOfServiceUrl = termsOfServiceUrl;
+	}
 
-    public void setContact(String contact) {
-        this.contact = contact;
-    }
+	public String getContact() {
+		return this.contact;
+	}
 
-    public String getLicense() {
-        return license;
-    }
+	public void setContact(String contact) {
+		this.contact = contact;
+	}
 
-    public void setLicense(String license) {
-        this.license = license;
-    }
+	public String getLicense() {
+		return this.license;
+	}
 
-    public String getLicenseUrl() {
-        return licenseUrl;
-    }
+	public void setLicense(String license) {
+		this.license = license;
+	}
 
-    public void setLicenseUrl(String licenseUrl) {
-        this.licenseUrl = licenseUrl;
-    }
+	public String getLicenseUrl() {
+		return this.licenseUrl;
+	}
 
-    public Info getInfo() {
-        return info;
-    }
+	public void setLicenseUrl(String licenseUrl) {
+		this.licenseUrl = licenseUrl;
+	}
 
-    public void setInfo(Info info) {
-        this.info = info;
-    }
+	public Info getInfo() {
+		return this.info;
+	}
 
-    public String getHost() {
-        return host;
-    }
+	public void setInfo(Info info) {
+		this.info = info;
+	}
 
-    public void setHost(String host) {
-        this.host = host;
-    }
+	public String getHost() {
+		return this.host;
+	}
 
-    public String getFilterClass() {
-        return filterClass;
-    }
+	public void setHost(String host) {
+		this.host = host;
+	}
 
-    public void setFilterClass(String filterClass) {
-        this.filterClass = filterClass;
-    }
+	@Override
+	public String getFilterClass() {
+		return this.filterClass;
+	}
 
-    public String getBasePath() {
-        return basePath;
-    }
+	public void setFilterClass(String filterClass) {
+		this.filterClass = filterClass;
+	}
 
-    public void setBasePath(String basePath) {
-        if (!"".equals(basePath) && basePath != null) {
-            if (!basePath.startsWith("/")) {
-                this.basePath = "/" + basePath;
-            } else {
-                this.basePath = basePath;
-            }
-        }
-    }
+	public String getBasePath() {
+		return this.basePath;
+	}
 
-    public void setPrettyPrint(String prettyPrint) {
-        if (prettyPrint != null) {
-            this.prettyPrint = Boolean.parseBoolean(prettyPrint);
-        }
-    }
+	public void setBasePath(String basePath) {
+		if (!"".equals(basePath) && (basePath != null)) {
+			if (!basePath.startsWith("/")) {
+				this.basePath = "/" + basePath;
+			} else {
+				this.basePath = basePath;
+			}
+		}
+	}
 
-    public boolean getScan() {
-        return true;
-    }
+	public void setPrettyPrint(String prettyPrint) {
+		if (prettyPrint != null) {
+			this.prettyPrint = Boolean.parseBoolean(prettyPrint);
+		}
+	}
 
-    public void setScan(boolean shouldScan) {
-        Set<Class<?>> classes = classes();
-        if (classes != null) {
-            Swagger swagger = reader.read(classes);
-            if (StringUtils.isNotBlank(host)) {
-                swagger.setHost(host);
-            }
+	public boolean getScan() {
+		return true;
+	}
 
-            if (StringUtils.isNotBlank(basePath)) {
-                swagger.setBasePath(basePath);
-            }
+	public void setScan(boolean shouldScan) {
+		Set<Class<?>> classes = this.classes();
+		if (classes != null) {
+			Swagger swagger = this.reader.read(classes);
+			if (StringUtils.isNotBlank(this.host)) {
+				swagger.setHost(this.host);
+			}
 
-            updateInfoFromConfig();
-        }
-        ScannerFactory.setScanner(this);
-    }
+			if (StringUtils.isNotBlank(this.basePath)) {
+				swagger.setBasePath(this.basePath);
+			}
 
-    public Set<Class<?>> classes() {
-        ConfigurationBuilder config = new ConfigurationBuilder();
-        Set<String> acceptablePackages = new HashSet<String>();
+			this.updateInfoFromConfig();
+		}
+		ScannerFactory.setScanner(this);
+	}
 
-        boolean allowAllPackages = false;
+	@Override
+	public Set<Class<?>> classes() {
+		ConfigurationBuilder config = new ConfigurationBuilder();
+		Set<String> acceptablePackages = new HashSet<String>();
 
-        if (resourcePackage != null && !"".equals(resourcePackage)) {
-            String[] parts = resourcePackage.split(",");
-            for (String pkg : parts) {
-                if (!"".equals(pkg)) {
-                    acceptablePackages.add(pkg);
-                    config.addUrls(ClasspathHelper.forPackage(pkg));
-                }
-            }
-        } else {
-            allowAllPackages = true;
-        }
+		boolean allowAllPackages = false;
 
-        config.setScanners(new ResourcesScanner(), new TypeAnnotationsScanner(), new SubTypesScanner());
+		if ((this.resourcePackage != null) && !"".equals(this.resourcePackage)) {
+			String[] parts = this.resourcePackage.split(",");
+			for (String pkg : parts) {
+				if (!"".equals(pkg)) {
+					acceptablePackages.add(pkg);
+					config.addUrls(ClasspathHelper.forPackage(pkg));
+				}
+			}
+		} else {
+			allowAllPackages = true;
+		}
 
-        final Reflections reflections = new Reflections(config);
-        Set<Class<?>> classes = reflections.getTypesAnnotatedWith(Api.class);
-        classes.addAll(reflections.getTypesAnnotatedWith(javax.ws.rs.Path.class));
-        classes.addAll(reflections.getTypesAnnotatedWith(SwaggerDefinition.class));
+		config.setScanners(new ResourcesScanner(), new TypeAnnotationsScanner(), new SubTypesScanner());
 
-        Set<Class<?>> output = new HashSet<Class<?>>();
-        for (Class<?> cls : classes) {
-            if (allowAllPackages) {
-                output.add(cls);
-            } else {
-                for (String pkg : acceptablePackages) {
-                    if (cls.getPackage().getName().startsWith(pkg)) {
-                        output.add(cls);
-                    }
-                }
-            }
-        }
-        return output;
-    }
+		final Reflections reflections = new Reflections(config);
+		Set<Class<?>> classes = reflections.getTypesAnnotatedWith(Api.class);
+		classes.addAll(reflections.getTypesAnnotatedWith(javax.ws.rs.Path.class));
+		classes.addAll(reflections.getTypesAnnotatedWith(SwaggerDefinition.class));
 
-    private void updateInfoFromConfig() {
-        info = getSwagger().getInfo();
-        if (info == null) {
-            info = new Info();
-        }
+		Set<Class<?>> output = new HashSet<Class<?>>();
+		for (Class<?> cls : classes) {
+			if (allowAllPackages) {
+				output.add(cls);
+			} else {
+				for (String pkg : acceptablePackages) {
+					if (cls.getPackage().getName().startsWith(pkg)) {
+						output.add(cls);
+					}
+				}
+			}
+		}
+		return output;
+	}
 
-        if (StringUtils.isNotBlank(description)) {
-            info.description(description);
-        }
+	private void updateInfoFromConfig() {
+		this.info = this.getSwagger().getInfo();
+		if (this.info == null) {
+			this.info = new Info();
+		}
 
-        if (StringUtils.isNotBlank(title)) {
-            info.title(title);
-        }
+		if (StringUtils.isNotBlank(this.description)) {
+			this.info.description(this.description);
+		}
 
-        if (StringUtils.isNotBlank(version)) {
-            info.version(version);
-        }
+		if (StringUtils.isNotBlank(this.title)) {
+			this.info.title(this.title);
+		}
 
-        if (StringUtils.isNotBlank(termsOfServiceUrl)) {
-            info.termsOfService(termsOfServiceUrl);
-        }
+		if (StringUtils.isNotBlank(this.version)) {
+			this.info.version(this.version);
+		}
 
-        if (contact != null) {
-            this.info.contact(new Contact()
-                    .name(contact));
-        }
-        if (license != null && licenseUrl != null) {
-            this.info.license(new License()
-                    .name(license)
-                    .url(licenseUrl));
-        }
-        if (schemes != null) {
-            for (String scheme : schemes) {
-                reader.getSwagger().scheme(Scheme.forValue(scheme));
-            }
-        }
+		if (StringUtils.isNotBlank(this.termsOfServiceUrl)) {
+			this.info.termsOfService(this.termsOfServiceUrl);
+		}
 
-        reader.getSwagger().setInfo(info);
-    }
+		if (this.contact != null) {
+			this.info.contact(new Contact()
+					.name(this.contact));
+		}
+		if ((this.license != null) && (this.licenseUrl != null)) {
+			this.info.license(new License()
+					.name(this.license)
+					.url(this.licenseUrl));
+		}
+		if (this.schemes != null) {
+			for (String scheme : this.schemes) {
+				this.reader.getSwagger().scheme(Scheme.forValue(scheme));
+			}
+		}
 
-    public Swagger getSwagger() {
-        return reader.getSwagger();
-    }
+		this.reader.getSwagger().setInfo(this.info);
+	}
 
-    public Swagger configure(Swagger swagger) {
-        if (schemes != null) {
-            for (String scheme : schemes) {
-                swagger.scheme(Scheme.forValue(scheme));
-            }
-        }
-        if (filterClass != null) {
-            try {
-                SwaggerSpecFilter filter = (SwaggerSpecFilter) Class.forName(filterClass).newInstance();
-                if (filter != null) {
-                    FilterFactory.setFilter(filter);
-                }
-            } catch (Exception e) {
-                LOGGER.error("failed to load filter", e);
-            }
-        }
-        return swagger.info(info)
-                .host(host)
-                .basePath(basePath);
-    }
+	public Swagger getSwagger() {
+		return this.reader.getSwagger();
+	}
+
+	@Override
+	public Swagger configure(Swagger swagger) {
+		if (this.schemes != null) {
+			for (String scheme : this.schemes) {
+				swagger.scheme(Scheme.forValue(scheme));
+			}
+		}
+		if (this.filterClass != null) {
+			try {
+				SwaggerSpecFilter filter = (SwaggerSpecFilter) Class.forName(this.filterClass).newInstance();
+				if (filter != null) {
+					FilterFactory.setFilter(filter);
+				}
+			} catch (Exception e) {
+				this.LOGGER.error("failed to load filter", e);
+			}
+		}
+		return swagger.info(this.info)
+				.host(this.host)
+				.basePath(this.basePath);
+	}
+
+	@Override
+	public boolean isScanAllResources() {
+		return this.isScanAllResources;
+	}
+
+	@Override
+	public Collection<String> getIgnoredRoutes() {
+		return this.ignoredRoutes;
+	}
+
+	public void setScanAllResources(boolean isScanAllResources) {
+		this.isScanAllResources = isScanAllResources;
+	}
+
+	/**
+	 * comma separated string, follow the same format as defined in
+	 * ServletConfig see ReaderConfigUtils.initReaderConfig
+	 * 
+	 * @param ignoredRoutes
+	 */
+	public void setIgnoredRoutes(String ignoredRoutes) {
+		if (ignoredRoutes == null) {
+			return;
+		}
+
+		for (String item : StringUtils.trimToEmpty(ignoredRoutes).split(",")) {
+			final String route = StringUtils.trimToNull(item);
+			if (route != null) {
+				this.ignoredRoutes.add(route);
+			}
+		}
+	}
 }
